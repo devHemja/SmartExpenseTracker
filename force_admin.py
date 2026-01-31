6241199b4cded3d9382ed767644a84f9627bb9b6
Hemja <hemja2003@gmail.com>
2026-02-01 02:02:41 +0530

    Forcing admin account creation

---
+++ force_admin.py
@@ -0,0 +1,23 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance_manager.settings')
+django.setup()
+
+from django.contrib.auth.models import User
+
+# Change these to whatever you want your "Boss" account to be
+username = 'sachin_boss'
+password = 'SecretPassword123' 
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, '', password)
+    print(f"Successfully created superuser: {username}")
+else:
+    # If the user exists, we will just reset the password to be sure
+    user = User.objects.get(username=username)
+    user.set_password(password)
+    user.is_staff = True
+    user.is_superuser = True
+    user.save()
+    print(f"Updated credentials for: {username}")
