068026c0eb92bd192a726acb4f66c0f6b55616e2
Hemja <hemja2003@gmail.com>
2026-02-01 01:45:09 +0530

    Migrated to Neon PostgreSQL

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -1,3 +1,5 @@
+import dj_database_url
+import os
 """
 Django settings for finance_manager project.
 
@@ -75,10 +77,12 @@ WSGI_APPLICATION = 'finance_manager.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # This looks for a variable named DATABASE_URL
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
